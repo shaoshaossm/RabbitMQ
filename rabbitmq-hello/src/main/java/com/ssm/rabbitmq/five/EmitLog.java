ddc4496a056e0ba0370313f6b54c31489c7eb99a
shaoshao <1600767556@qq.com>
2022-11-22 19:47:02 +0800

    fanout交换机

--- rabbitmq-hello/src/main/java/com/ssm/rabbitmq/five/EmitLog.java
+++ rabbitmq-hello/src/main/java/com/ssm/rabbitmq/five/EmitLog.java
@@ -1,9 +1,28 @@
 package com.ssm.rabbitmq.five;
 
+import com.rabbitmq.client.Channel;
+import com.ssm.rabbitmq.utils.RabbitMqUtils;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * @author shaoshao
  * @Date 2022/11/22 19:34
  * @Description:
  */
 public class EmitLog {
+    public static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        // 声明交换机 消费者声明了这里可以不声明
+//        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String message = scanner.next();
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println("生产者发出消息：" + message);
+        }
+    }
 }
---
+++ rabbitmq-hello/src/main/java/com/ssm/rabbitmq/five/ReceiveLogs02.java
@@ -0,0 +1,33 @@
+package com.ssm.rabbitmq.five;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Delivery;
+import com.ssm.rabbitmq.utils.RabbitMqUtils;
+
+/**
+ * @author shaoshao
+ * @Date 2022/11/22 19:20
+ * @Description:
+ */
+public class ReceiveLogs02 {
+
+    public static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        // 声明交换机
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        // 声明一个临时队列 当消费者与队列断开时，队列自动删除
+        String queueName = channel.queueDeclare().getQueue();
+        // 绑定交换机与队列
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        DeliverCallback deliverCallback = (String consumerTag, Delivery message) -> {
+            System.out.println("ReceiveLogs02"+new String(message.getBody()));
+        };
+
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+
+    }
+}
