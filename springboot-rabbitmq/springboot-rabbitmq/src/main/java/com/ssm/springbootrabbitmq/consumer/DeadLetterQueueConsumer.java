1502b9c5c8444b647d0baa42a59a9320475ce58e
shaoshao <1600767556@qq.com>
2022-11-24 22:42:58 +0800

    死信队列消费者

---
+++ springboot-rabbitmq/springboot-rabbitmq/src/main/java/com/ssm/springbootrabbitmq/consumer/DeadLetterQueueConsumer.java
@@ -0,0 +1,26 @@
+package com.ssm.springbootrabbitmq.consumer;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author shaoshao
+ * @Date 2022/11/24 19:10
+ * @Description: 队列TTL 消费者
+ */
+@Slf4j
+@Component
+public class DeadLetterQueueConsumer {
+
+    @RabbitListener(queues = "QD")
+    public void receiveD(Message message, Channel channel) throws Exception {
+        String msg = new String(message.getBody());
+        log.info("当前时间：{} ，收到死信队列消息：{}", new Date().toString(), msg);
+    }
+
+}
