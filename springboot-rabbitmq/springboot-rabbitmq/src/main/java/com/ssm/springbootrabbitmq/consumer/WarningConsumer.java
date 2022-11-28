6630e82b0b55587e11fc4ab99ade75bd4cf657e0
shaoshao <1600767556@qq.com>
2022-11-28 19:51:26 +0800

    报警消费者

---
+++ springboot-rabbitmq/springboot-rabbitmq/src/main/java/com/ssm/springbootrabbitmq/consumer/WarningConsumer.java
@@ -0,0 +1,25 @@
+package com.ssm.springbootrabbitmq.consumer;
+
+
+import com.ssm.springbootrabbitmq.config.ConfirmConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author shaoshao
+ * @Date 2022/11/27 14:04
+ * @Description: 报警消费者
+ */
+@Slf4j
+@Component
+public class WarningConsumer {
+
+    //接收报警信息
+    @RabbitListener(queues = ConfirmConfig.WARNING_QUEUE_NAME)
+    public void receiveWarningMsg(Message message){
+        String msg = new String(message.getBody());
+        log.error("报警发现不可路由消息:{}",msg);
+    }
+}
